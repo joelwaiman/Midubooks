0cd889ec0ac7aa9895bed4c11cbea888d229ffb3
joelwaiman <waimanjoel@gmail.com>
2023-08-22 15:10:00 -0300

    empty fav

--- src/app/favorites/page.jsx
+++ src/app/favorites/page.jsx
@@ -3,23 +3,26 @@
 import styles from './page.module.css'
 import { useReadList } from "../ContextAPI"
 
-export default function Favorites(){
+export default function Favorites() {
 
-    const {readList} = useReadList();
+  const { readList } = useReadList();
 
-    return(
-        <article className={styles.article}>
-      <ul className={styles.ul}>
-        {readList.map((book) => {
-          return <li className={styles.book} key={book.ISBN}>
-            <img className={styles.img}
-              alt={book.title}
-              src={book.cover}>
-            </img>
-            <p className={styles.fav}>{book.title}</p>
-          </li>
-        })}
-      </ul>
+  return (
+    <article className={styles.article}>
+      {readList.length === 0 ?
+        <h1>Johi puta</h1>
+        :
+        <ul className={styles.ul}>
+          {readList.map((book) => {
+            return <li className={styles.book} key={book.ISBN}>
+              <img className={styles.img}
+                alt={book.title}
+                src={book.cover}>
+              </img>
+              <p className={styles.fav}>{book.title}</p>
+            </li>
+          })}
+        </ul>}
     </article>
-    )
+  )
 }
